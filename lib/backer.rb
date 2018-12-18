57368a3b4fe99ec71bbfbc7ca4c215efb1d83afc
Paula McKinley <pmknyc@optimum.net>
2018-12-18 18:37:26 +0000

    Done.

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,9 +2,10 @@ class Backer
 
 attr_accessor :name, :backed_projects
 
+
 	def initialize(name)
     @name = name
-		@backed_projects = []
+    @backed_projects = []
 	end
 
 	def back_project(project)
